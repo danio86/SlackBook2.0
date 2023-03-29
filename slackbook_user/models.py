53d241de566a418979b945d9336c8ca6290afafb
danio86 <d.harder@gmx.at>
2023-03-29 09:23:50 +0000

    add manuel usermodel

--- slackbook_user/models.py
+++ slackbook_user/models.py
@@ -6,6 +6,8 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True)
     biography = models.TextField(null=True)
+    id = models.AutoField(primary_key=True)
+    # avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
