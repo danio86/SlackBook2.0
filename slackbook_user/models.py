f50ebaaec2db41d18c4987690de0ce258d4b64ae
danio86 <d.harder@gmx.at>
2023-04-04 08:42:17 +0000

    add change private status to create, update channel

--- slackbook_user/models.py
+++ slackbook_user/models.py
@@ -41,9 +41,8 @@ class Channel(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    privat = models.BooleanField(default=True)
     keywords = models.TextField(blank=True)
-    private = models.BooleanField(default=True)
+    private = models.BooleanField(default=True, blank=True)
     permission = models.TextField(blank=True)
     members = models.ManyToManyField(
         User, blank=True, related_name='members',
