e025be1218b42bc9b03b9a60e64a22ee494ecccf
danio86 <>
2023-04-05 17:00:26 +0000

    add the hosted channels and the joined channels to index.html

--- slackbook_user/views.py
+++ slackbook_user/views.py
@@ -20,7 +20,7 @@ def home(request):
 
     comments = Post.objects.all().order_by('-created_on').filter(
         Q(channel__topic__title__icontains=s))
-        # Q(channel__title__icontains=s))
+    # Q(channel__title__icontains=s))
     # this filters by the title of the topic of the channel
     # if the url ending is in channel title
 
