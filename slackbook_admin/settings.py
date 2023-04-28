b6c97e00eb6e796a500e39ef637d384f38ea83d9
danio86 <>
2023-04-28 16:21:12 +0000

    add staticfiles folder

--- slackbook_admin/settings.py
+++ slackbook_admin/settings.py
@@ -154,6 +154,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 MEDIA_URL = '/images/'
