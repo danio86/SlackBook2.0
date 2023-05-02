e87214caef8e8444957166f445bbca35021c7aeb
danio86 <d.harder@gmx.at>
2023-05-02 11:49:39 +0000

    removed form action error

--- slackbook_admin/settings.py
+++ slackbook_admin/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # 2 hosts (heroku and local)
 ALLOWED_HOSTS = ['slackbook2.herokuapp.com', 'localhost',
